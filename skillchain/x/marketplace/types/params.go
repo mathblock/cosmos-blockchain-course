6745701a711e0706809d66249c333ad45396605b
MatVD <vandammemathieu@hotmail.fr>
2025-12-06 08:49:20 +0100

    feat: add parameters for platform fee, contract duration, and minimum gig price

--- skillchain/x/marketplace/types/params.go
+++ skillchain/x/marketplace/types/params.go
@@ -1,17 +1,46 @@
 package types
 
+import (
+	"fmt"
+    "cosmossdk.io/math"
+)
+
+// Default parameter values
+var (
+    DefaultPlatformFeePercent  = uint64(5)           // 5%
+    DefaultMinContractDuration = uint64(86400)       // 1 days in secondes
+    DefaultMinGigPrice         = math.NewInt(1)  	 // 1 SKILL
+)
+
 // NewParams creates a new Params instance.
-func NewParams() Params {
-	return Params{}
+func NewParams(feePercent, minDuration uint64, minPrice math.Int) Params {
+    return Params{
+        PlatformFeePercent:  feePercent,
+        MinContractDuration: minDuration,
+        MinGigPrice:         minPrice,
+    }
 }
 
 // DefaultParams returns a default set of parameters.
 func DefaultParams() Params {
-	return NewParams()
+    return NewParams(
+        DefaultPlatformFeePercent,
+        DefaultMinContractDuration,
+        DefaultMinGigPrice,
+    )
 }
 
 // Validate validates the set of params.
 func (p Params) Validate() error {
-
-	return nil
+    if p.PlatformFeePercent > 100 {
+        return fmt.Errorf("platform fee cannot exceed 100%")
+    }
+    if p.MinContractDuration == 0 {
+		return fmt.Errorf("min contract duration (in seconds) must be greater than zero")
+	}
+    if p.MinGigPrice.IsNegative() {
+        return fmt.Errorf("min gig price must be a positive integer")
+    }
+    
+    return nil
 }
