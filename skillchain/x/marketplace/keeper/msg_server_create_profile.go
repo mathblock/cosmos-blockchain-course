48099b5ddbf250b36c70d448cda29a9abd01d61e
MatVD <vandammemathieu@hotmail.fr>
2025-12-06 21:49:34 +0100

    feat: implement profile creation logic with validation

--- skillchain/x/marketplace/keeper/msg_server_create_profile.go
+++ skillchain/x/marketplace/keeper/msg_server_create_profile.go
@@ -2,18 +2,64 @@ package keeper
 
 import (
 	"context"
+	"errors"
 
 	"skillchain/x/marketplace/types"
 
 	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-func (k msgServer) CreateProfile(ctx context.Context, msg *types.MsgCreateProfile) (*types.MsgCreateProfileResponse, error) {
+func (k msgServer) CreateProfile(goCtx context.Context, msg *types.MsgCreateProfile) (*types.MsgCreateProfileResponse, error) {
 	if _, err := k.addressCodec.StringToBytes(msg.Creator); err != nil {
 		return nil, errorsmod.Wrap(err, "invalid authority address")
 	}
 
 	// TODO: Handle the message
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	_, err := k.Profile.Get(ctx, msg.Creator)
+	if err == nil {
+		// Profile exists
+		return nil, errorsmod.Wrapf(sdkerrors.ErrInvalidRequest, "profile for creator %s already exists", msg.Creator)
+	} else if !errors.Is(err, sdkerrors.ErrKeyNotFound) {
+		// Some other error occurred
+		return nil, errorsmod.Wrapf(err, "failed to check existing profile for creator %s", msg.Creator)
+	}
+
+	if msg.HourlyRate < 1 {
+        return nil, errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "hourly rate must be at least 1 skill")
+    }
+
+	if len(msg.Skills) == 0 {
+        return nil, errorsmod.Wrap(sdkerrors.ErrInvalidRequest, "at least one skill is required")
+    }
+
+	profile := types.Profile{
+        Owner:       msg.Creator,
+        Name:        msg.Name,
+        Bio:         msg.Bio,
+        Skills:      msg.Skills,
+        HourlyRate:  msg.HourlyRate,
+        TotalJobs:   0,
+        TotalEarned: 0,
+        RatingSum:   0,
+        RatingCount: 0,
+    }
+
+	err = k.Profile.Set(ctx, profile.Owner, profile)
+	if err != nil {
+		return nil, errorsmod.Wrapf(err, "failed to create profile for creator %s", msg.Creator)
+	}
+
+	ctx.EventManager().EmitEvent(
+        sdk.NewEvent(
+            "profile_created",
+            sdk.NewAttribute("owner", msg.Creator),
+            sdk.NewAttribute("name", msg.Name),
+        ),
+    )
 
 	return &types.MsgCreateProfileResponse{}, nil
 }
