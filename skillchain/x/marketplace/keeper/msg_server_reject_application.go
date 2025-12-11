4de264c2b2739cf95085f4713591d6ac6b9b9b56
MatVD <vandammemathieu@hotmail.fr>
2025-12-11 16:01:25 +0100

    feat: enhance RejectApplication and WithdrawApplication functions

--- skillchain/x/marketplace/keeper/msg_server_reject_application.go
+++ skillchain/x/marketplace/keeper/msg_server_reject_application.go
@@ -2,18 +2,58 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	"skillchain/x/marketplace/types"
-
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	errorsmod "cosmossdk.io/errors"
 )
 
-func (k msgServer) RejectApplication(ctx context.Context, msg *types.MsgRejectApplication) (*types.MsgRejectApplicationResponse, error) {
+func (k msgServer) RejectApplication(goCtx context.Context, msg *types.MsgRejectApplication) (*types.MsgRejectApplicationResponse, error) {
 	if _, err := k.addressCodec.StringToBytes(msg.Creator); err != nil {
 		return nil, errorsmod.Wrap(err, "invalid authority address")
 	}
 
 	// TODO: Handle the message
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	application, err := k.Application.Get(ctx, msg.ApplicationId)
+	if err != nil {
+		return nil, errorsmod.Wrapf(sdkerrors.ErrNotFound, "application with id %d not found: %v", msg.ApplicationId, err)
+	}
+
+	gig, err := k.Gig.Get(ctx, application.GigId)
+	if err != nil {
+		return nil, errorsmod.Wrapf(sdkerrors.ErrNotFound, "gig with id %d not found: %v", application.GigId, err)
+	}
+
+	if gig.Owner != msg.Creator {
+		return nil, errorsmod.Wrap(types.ErrUnauthorized, "only the owner can reject applications for their gig")
+	}
+
+	if application.Status != "pending" {
+        return nil, errorsmod.Wrapf(
+            sdkerrors.ErrInvalidRequest,
+            "can only reject pending applications (current status: %s)",
+            application.Status,
+        )
+    }
+
+	application.Status = "rejected"
+	err = k.Application.Set(ctx, application.Id, application)
+	if err != nil {
+		return nil, errorsmod.Wrapf(sdkerrors.ErrUnknownRequest, "failed to update application status: %v", err)
+	}
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			"application_rejected",
+			sdk.NewAttribute("application_id", fmt.Sprintf("%d", application.Id)),
+			sdk.NewAttribute("owner", gig.Owner),
+			sdk.NewAttribute("gig_id", fmt.Sprintf("%d", application.GigId)),
+		),
+	)
 
 	return &types.MsgRejectApplicationResponse{}, nil
 }
--- skillchain/x/marketplace/keeper/msg_server_withdraw_application.go
+++ skillchain/x/marketplace/keeper/msg_server_withdraw_application.go
@@ -2,18 +2,52 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	"skillchain/x/marketplace/types"
-
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	errorsmod "cosmossdk.io/errors"
 )
 
-func (k msgServer) WithdrawApplication(ctx context.Context, msg *types.MsgWithdrawApplication) (*types.MsgWithdrawApplicationResponse, error) {
+func (k msgServer) WithdrawApplication(goCtx context.Context, msg *types.MsgWithdrawApplication) (*types.MsgWithdrawApplicationResponse, error) {
 	if _, err := k.addressCodec.StringToBytes(msg.Creator); err != nil {
 		return nil, errorsmod.Wrap(err, "invalid authority address")
 	}
 
 	// TODO: Handle the message
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	application, err := k.Application.Get(ctx, msg.ApplicationId)
+	if err != nil {
+		return nil, errorsmod.Wrapf(sdkerrors.ErrNotFound, "application with id %d not found: %v", msg.ApplicationId, err)
+	}
+
+	if application.Freelancer != msg.Creator {
+        return nil, errorsmod.Wrap(types.ErrUnauthorized, "only the freelancer can withdraw their application")
+    }
+
+	if application.Status != "pending" {
+        return nil, errorsmod.Wrapf(
+            sdkerrors.ErrInvalidRequest,
+            "can only withdraw pending applications (current status: %s)",
+            application.Status,
+        )
+    }
+
+	application.Status = "withdrawn"
+	err = k.Application.Set(ctx, application.Id, application)
+	if err != nil {
+		return nil, errorsmod.Wrapf(sdkerrors.ErrUnknownRequest, "failed to update application status: %v", err)
+	}
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			"application_withdrawn",
+			sdk.NewAttribute("application_id", fmt.Sprintf("%d", application.Id)),
+			sdk.NewAttribute("freelancer", application.Freelancer),
+		),
+	)
 
 	return &types.MsgWithdrawApplicationResponse{}, nil
 }
