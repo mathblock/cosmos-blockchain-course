bd430367d2ba8d242427f56d4e136024ee1c1e3e
MatVD <vandammemathieu@hotmail.fr>
2025-12-12 19:14:01 +0100

    feat: implement EscrowBalance query logic and response handling

--- skillchain/x/marketplace/keeper/query_escrow_balance.go
+++ skillchain/x/marketplace/keeper/query_escrow_balance.go
@@ -5,16 +5,21 @@ import (
 
 	"skillchain/x/marketplace/types"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func (q queryServer) EscrowBalance(ctx context.Context, req *types.QueryEscrowBalanceRequest) (*types.QueryEscrowBalanceResponse, error) {
+func (q queryServer) EscrowBalance(goCtx context.Context, req *types.QueryEscrowBalanceRequest) (*types.QueryEscrowBalanceResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
 	// TODO: Process the query
+	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	return &types.QueryEscrowBalanceResponse{}, nil
+	addr := q.k.accountKeeper.GetModuleAddress(types.ModuleName)
+	balance := q.k.bankKeeper.GetBalance(ctx, addr, "skill")
+
+	return &types.QueryEscrowBalanceResponse{Balance: &balance}, nil
 }
