fe13d8e1175401a473525cc388b34802ce50598f
MatVD <vandammemathieu@hotmail.fr>
2025-12-12 12:34:50 +0100

    feat: enhance expected interfaces for AuthKeeper and BankKeeper, and add EscrowAccountName constant

--- skillchain/x/marketplace/types/keys.go
+++ skillchain/x/marketplace/types/keys.go
@@ -9,6 +9,9 @@ const (
 	// StoreKey defines the primary module store key
 	StoreKey = ModuleName
 
+	// EscrowAccountName is the name of the escrow module account
+	EscrowAccountName = "marketplace_escrow"
+
 	// GovModuleName duplicates the gov module's name to avoid a dependency with x/gov.
 	// It should be synced with the gov module's name if it is ever changed.
 	// See: https://github.com/cosmos/cosmos-sdk/blob/v0.52.0-beta.2/x/gov/types/keys.go#L9
