a0ce9fd359d0ede56ebae1bdadfffd66be6e44e1
MatVD <vandammemathieu@hotmail.fr>
2025-12-12 08:43:01 +0100

    feat: implement ApplicationsByFreelancer query

--- skillchain/x/marketplace/keeper/query_applications_by_freelancer.go
+++ skillchain/x/marketplace/keeper/query_applications_by_freelancer.go
@@ -4,17 +4,30 @@ import (
 	"context"
 
 	"skillchain/x/marketplace/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func (q queryServer) ApplicationsByFreelancer(ctx context.Context, req *types.QueryApplicationsByFreelancerRequest) (*types.QueryApplicationsByFreelancerResponse, error) {
+func (q queryServer) ApplicationsByFreelancer(goCtx context.Context, req *types.QueryApplicationsByFreelancerRequest) (*types.QueryApplicationsByFreelancerResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
 	// TODO: Process the query
+	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	return &types.QueryApplicationsByFreelancerResponse{}, nil
+	var applications []types.Application
+	err := q.k.Application.Walk(ctx, nil, func(key uint64, application types.Application) (stop bool, err error) {
+		if application.Freelancer == req.Freelancer {
+			applications = append(applications, application)
+		}
+		return false, nil
+	})
+	if err != nil {
+		return nil, status.Error(codes.Internal, "failed to retrieve applications")
+	}
+
+	return &types.QueryApplicationsByFreelancerResponse{Applications: applications}, nil
 }
