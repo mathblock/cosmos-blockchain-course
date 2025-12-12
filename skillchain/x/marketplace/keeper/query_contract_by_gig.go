cbf1f5be8ca43571a9fce3ea46573393789a5ad0
MatVD <vandammemathieu@hotmail.fr>
2025-12-12 08:40:15 +0100

    feat: enhance ContractByGig query to return contract details

--- skillchain/x/marketplace/keeper/query_contract_by_gig.go
+++ skillchain/x/marketplace/keeper/query_contract_by_gig.go
@@ -4,17 +4,23 @@ import (
 	"context"
 
 	"skillchain/x/marketplace/types"
-
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func (q queryServer) ContractByGig(ctx context.Context, req *types.QueryContractByGigRequest) (*types.QueryContractByGigResponse, error) {
+func (q queryServer) ContractByGig(goCtx context.Context, req *types.QueryContractByGigRequest) (*types.QueryContractByGigResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
 	// TODO: Process the query
+	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	return &types.QueryContractByGigResponse{}, nil
+	contract, err := q.k.Contract.Get(ctx, req.GigId)
+	if err != nil {
+		return nil, status.Error(codes.NotFound, "contract not found")
+	}
+
+	return &types.QueryContractByGigResponse{Contract: &contract}, nil
 }
