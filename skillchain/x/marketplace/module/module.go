bb6b40d0d63881aeac51607353b6f5a6ae662f00
MatVD <vandammemathieu@hotmail.fr>
2025-12-14 10:33:50 +0100

    feat(dispute): implement EndBlock logic to process expired disputes

--- skillchain/x/marketplace/module/module.go
+++ skillchain/x/marketplace/module/module.go
@@ -138,6 +138,7 @@ func (am AppModule) BeginBlock(_ context.Context) error {
 
 // EndBlock contains the logic that is automatically triggered at the end of each block.
 // The end block implementation is optional.
-func (am AppModule) EndBlock(_ context.Context) error {
-	return nil
+func (am AppModule) EndBlock(ctx context.Context) error {
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	return am.keeper.ProcessExpiredDisputes(sdkCtx)
 }
